7384f8ec8ff4a3e30684ce32c093069fe9ab38ae
yeonfish6040 <72007187+yeonfish6040@users.noreply.github.com>
2022-09-24 23:07:08 +0900

    update

--- lab/src/sha256_decoder/main.java
+++ lab/src/sha256_decoder/main.java
@@ -14,39 +14,59 @@ import util.Stopwatch;
 public class main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        Scanner scF = new Scanner(new File("shaKeys.db"));
-        String StartStr = "";
-        while (scF.hasNextLine()) {
-            StartStr = scF.nextLine();
-        }
-        System.out.println(StartStr);
-        StartStr = StartStr.split("!=!")[0];
-        System.out.println(StartStr);
-        String[] strList = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", "|", "\\", ":", ";", "\'", "\"", ",", "<", ".", ">", "/", "?", " "};
-        StringBuffer lines = new StringBuffer();
-        String strTemp = StartStr;
         System.out.print("몇줄이나 쓸겨? ");
         int i = sc.nextInt();
+        Boolean go = false;
         Stopwatch.Flag();
         while(true) {
-            System.out.println(i);
-            strTemp = strIncrease(strTemp, strList);
-            if (strTemp.equals("!=!"))
-                continue;
-            try{
-                String line = strTemp+"!=!"+SHA256.encrypt(strTemp)+"\n";
-                lines.append(line);
-            }catch (Exception e){
+            Scanner scF = new Scanner(new File("shaKeys.db"));
+            String StartStr = "";
+            while (scF.hasNextLine()) {
+                StartStr = scF.nextLine();
             }
-            i--;
-            if (i == 0)
+            System.out.println(StartStr);
+            StartStr = StartStr.split("!=!")[0];
+            System.out.println(StartStr);
+            String[] strList = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", "|", "\\", ":", ";", "\'", "\"", ",", "<", ".", ">", "/", "?", " "};
+            StringBuffer lines = new StringBuffer();
+            String strTemp = StartStr;
+            int step;
+            if(i>10000000){
+                step = 10000000;
+                i -= 10000000;
+            }else {
+                step = i;
+                i = 0;
+                go = true;
+            }
+            Stopwatch.Flag();
+            while(true) {
+                System.out.println(i+step);
+                strTemp = strIncrease(strTemp, strList);
+                if (strTemp.equals("!=!"))
+                    continue;
+                try{
+                    String line = strTemp+"!=!"+SHA256.encrypt(strTemp)+"\n";
+                    lines.append(line);
+                }catch (Exception e){
+                }
+                step--;
+                if (step == 0)
+                    break;
+            }
+            Stopwatch.Flag();
+            FileWriter fr = new FileWriter("shaKeys.db", true);
+            fr.write(lines.toString());
+            fr.close();
+            if(go) {
                 break;
+            }
         }
-        FileWriter fr = new FileWriter("shaKeys.db", true);
-        fr.write(lines.toString());
-        fr.close();
         Stopwatch.Flag();
-        System.out.println(Stopwatch.getDuration(0, 1)+"ms was taken for this work");
+        System.out.println(Stopwatch.getDuration(0,3)+"ms was taken for this work");
+        System.out.println(Stopwatch.getDuration(0,1)+"ms was taken for read db file");
+        System.out.println(Stopwatch.getDuration(1,2)+"ms was taken for making");
+        System.out.println(Stopwatch.getDuration(2, 3)+"ms was taken for saving result");
     }
 
     private static String strIncrease(String str, String[] strList) {

0e2b70d112adc85365e4cb03c68312e78e3b7a97
yeonfish6040 <72007187+yeonfish6040@users.noreply.github.com>
2022-10-21 00:57:22 +0900

    update

---
+++ SpringBoard/src/test/java/com/lyj_web/springboard/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyj_web.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
